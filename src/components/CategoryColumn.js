e61b4a5223f95361c898163c52805ccd8a6b523c
Ben Biederman <benbiederman@gmail.com>
2022-01-04 06:34:25 -0600

    Add Category Data based on header month.

--- src/components/CategoryColumn.js
+++ src/components/CategoryColumn.js
@@ -1,21 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CategoryData from "./CategoryData";
 import ColumnHeader from "./ColumnHeader";
 
 const CategoryColumn = (props) => {
+    // State
+    const[housing, setHousing] = useState();
+    const[transportation, setTransportation] = useState();
+    const[food, setFood] = useState();
+    const[utilities, setUtilities] = useState();
+    const[medical, setMedical] = useState();
+    const[recreation, setRecreation] = useState();
+    const[misc, setMisc] = useState();
+
+    useEffect(() => {
+        setHousing(0);
+        setTransportation(0);
+        setFood(0);
+        setUtilities(0);
+        setMedical(0);
+        setRecreation(0);
+        setMisc(0);
+
+        props.data.map((d) => {
+            if(d.category === 'housing'){
+                setHousing(prevState => prevState + d.amount);
+            } else if(d.category === 'transportation'){
+                setTransportation(prevState => prevState + d.amount);
+            } else if(d.category === 'food'){
+                setFood(prevState => prevState + d.amount);
+            } else if(d.category === 'utilities'){
+                setUtilities(prevState => prevState + d.amount);
+            } else if(d.medical === 'medical'){
+                setMedical(prevState => prevState + d.amount);
+            } else if(d.medical === 'recreation'){
+                setRecreation(prevState => prevState + d.amount);
+            } else{
+                setMisc(prevState => prevState + d.amount);
+            }
+        })
+    }, [props.data])
+
     
 
     return (
         <section className="display-column">
             <ColumnHeader header={props.header} />
             <div className="column-data">
-                <CategoryData header={"Housing"} amount={1234.00} />
-                <CategoryData header={"Transportation"} amount={1234} />
-                <CategoryData header={"Food"} amount={1234.00} />
-                <CategoryData header={"Utilities"} amount={1234} />
-                <CategoryData header={"Medical"} amount={1234.00} />
-                <CategoryData header={"Recreation"} amount={1234} />
-                <CategoryData header={"Misc."} amount={1234} />
+                <CategoryData header={"Housing"} amount={housing.toFixed(2)} />
+                <CategoryData header={"Transportation"} amount={transportation.toFixed(2)} />
+                <CategoryData header={"Food"} amount={food.toFixed(2)} />
+                <CategoryData header={"Utilities"} amount={utilities.toFixed(2)} />
+                <CategoryData header={"Medical"} amount={medical.toFixed(2)} />
+                <CategoryData header={"Recreation"} amount={recreation.toFixed(2)} />
+                <CategoryData header={"Misc."} amount={misc.toFixed(2)} />
             </div>
         </section>
     )
--- src/components/CategoryData.js
+++ src/components/CategoryData.js
@@ -2,7 +2,7 @@ const CategoryData = (props) => {
     return (
         <div className="data">
             <h3>{props.header}</h3>
-            <p>${props.amount.toFixed(2)}</p>
+            <p>${props.amount}</p>
         </div>
     )
 }
