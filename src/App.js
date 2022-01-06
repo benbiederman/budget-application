3f455b1f19390009082331799adde6d4c5b15314
Ben Biederman <benbiederman@gmail.com>
2022-01-06 06:52:20 -0600

    Update to pull data from local storage and load new data entry to local storage.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import AddSection from './components/AddSection';
-import DisplaySection from './components/DisplaySection';
-import './styles/app.css';
+import React, { useState, useEffect } from "react";
+import AddSection from "./components/AddSection";
+import DisplaySection from "./components/DisplaySection";
+import "./styles/app.css";
 
 function App() {
   // State
-  const[income, setIncome] = useState([]);
-  const[expense, setExpense] = useState([]);
+  const [income, setIncome] = useState(loadLocalData("income"));
+  const [expense, setExpense] = useState(loadLocalData("expense"));
 
   // Functions
   // Add item to Income
-  function addIncome(data){
-      setIncome([...income, data]);
+  function addIncome(data) {
+    setIncome([...income, data]);
   }
 
   //Add item to Expense
-  function addExpense(data){
-      setExpense([...expense, data])
-  }   
+  function addExpense(data) {
+    setExpense([...expense, data]);
+  }
 
   // Remove items from Income Data
-  function removeIncome(id){
+  function removeIncome(id) {
     setIncome(
-        income.filter(i => {
-            return i.id !== id;
-        })
-    )
+      income.filter((i) => {
+        return i.id !== id;
+      })
+    );
   }
 
   // Remove items from Expense Data
-  function removeExpense(id){
-     setExpense(
-        expense.filter(e => {
-            return e.id !== id;
-        })
-    )
+  function removeExpense(id) {
+    setExpense(
+      expense.filter((e) => {
+        return e.id !== id;
+      })
+    );
   }
+
+  // Load data from local storage
+  function loadLocalData(data) {
+    const localData = localStorage.getItem(data);
+    return localData ? JSON.parse(localData) : [];
+  }
+
+  // Save data to local storage
+  // Income
+  useEffect(() => {
+    localStorage.setItem("income", JSON.stringify(income));
+  }, [income]);
+
+  // Expense
+  useEffect(() => {
+    localStorage.setItem("expense", JSON.stringify(expense));
+  }, [expense]);
 
   return (
     <div className="app">
-        <AddSection 
-            addIncome={addIncome}
-            addExpense={addExpense}
-        />
-        <DisplaySection 
-          income={income}
-          expense={expense}
-          removeIncome={removeIncome}
-          removeExpense={removeExpense}
-        />
+      <AddSection addIncome={addIncome} addExpense={addExpense} />
+      <DisplaySection
+        income={income}
+        expense={expense}
+        removeIncome={removeIncome}
+        removeExpense={removeExpense}
+      />
     </div>
   );
 }
